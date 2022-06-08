47f557063f43a8b321fb213e91dd4cb29b35d293
MitchDom <dominiak.michel@gmail.com>
2022-06-08 15:33:03 +0200

    création schéma de données, création, modification, suppression sauce

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // on importe Express
 const express = require('express');
 const bodyParser = require('body-parser');
+// importation de mogoose
+const mongoose = require('mongoose');
+
+const Sauce = require('./models/Sauce');
 
 // création d'une constante app qui sera notre application
 
 const app = express();
 
+mongoose.connect('mongodb+srv://MitchDom:DB202206@cluster0.ssdtd.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser : true,
+  useUnifiedTopology: true})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => {
+    console.log(error);
+    console.log('Connexion à MongoDB échouée !')
+  });
 
 app.use(express.json());
 
@@ -18,30 +30,38 @@ app.use((req, res, next) => {
   });
 
 app.post('/api/sauce', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message : 'sauce créée !'
+    delete req.body._id;
+    const sauce = new Sauce({
+        ...req.body
     });
+    sauce.save()
+        .then(() => res.status(201).json({ messsage : 'Sauce créée !'}))
+        .catch(error => res.status(400).json({ error }));
+    
+});
+
+app.put('/api/sauce/:id', (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id}, { ...req.body, _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/sauce/:id', (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/sauce/:id', (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id})
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
 });
 
 app.get('/api/sauce', (req, res, next) => {
-    const sauce = [
-      {
-        name: '',
-        manufacturer: '',
-        description : '',
-        heat: '',
-        likes: '',
-        dislikes: '',
-        imageUrl: '',
-        mainPepper: '',
-        usersLiked: '',
-        usersDisliked: '',
-        userId: '',
-      },
-
-    ];
-    res.status(200).json(stuff);
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
   });
 
 module.exports = app;
