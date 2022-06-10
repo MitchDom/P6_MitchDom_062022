cc54daedca3b84ff420416089135ca2a7ead9105
MitchDom <dominiak.michel@gmail.com>
2022-06-10 11:31:03 +0200

    optimisation backend

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 // importation de mogoose
 const mongoose = require('mongoose');
 
-const Sauce = require('./models/Sauce');
+const sauceRoutes = require('./routes/sauce');
 
 // création d'une constante app qui sera notre application
 
@@ -29,42 +29,9 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/sauce', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-        ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({ messsage : 'Sauce créée !'}))
-        .catch(error => res.status(400).json({ error }));
-    
-});
+app.use(bodyParser.json());
 
-app.put('/api/sauce/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id}, { ...req.body, _id: req.params.id})
-    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/sauce/:id', (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauce/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id})
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/sauce', (req, res, next) => {
-    Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-  });
-
-module.exports = app;
+app.use('/api/sauce', sauceRoutes);
 
 // on exporte cette constante pour pouvoir y accéder depuis les autres fichiers du projet, notamment le serveur node
 module.exports = app;
